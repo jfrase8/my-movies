092ebde00bed8cc12da96a34fe7571cbc16d5054
Thomas Kendall <thomas.j.kendall@gmail.com>
2018-03-12 19:58:39 -0500

    Added initial project structure

---
+++ my-movies-web/src/main/java/com/jfrase8/mymovies/web/MyMoviesWebApplication.java
@@ -0,0 +1,12 @@
+package com.jfrase8.mymovies.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesWebApplication.class, args);
+	}
+}
---
+++ my-movies-web/src/test/java/com/jfrase8/mymovies/web/MyMoviesWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfrase8.mymovies.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyMoviesWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
